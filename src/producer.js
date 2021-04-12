15693a8a5270d5371c72f1c2507f03c44e9d6b75
robby <lomiver@gmail.com>
2021-04-12 12:25:48 +0700

    edit random

--- src/producer.js
+++ src/producer.js
@@ -12,16 +12,15 @@ const topic = config.kafka.TOPIC
 const producer = client.producer()
 
 const randomAccount = async(producer, topic) =>{
+  await producer.connect()
   let getAll = await axios.get('http://localhost:3000/getAll')
   let randomData = []
   getAll.data.forEach(number => {randomData.push(number.accountNumber)})
-  console.log(randomData)
-  await producer.connect()
-  console.log('access this')
   let i = 1
   setInterval(async function() {
-    i = i >= getAll.data.length() ? 0 : i + 1
-    const data = await axios.get(`http://localhost:3000/account/${randomData}`)
+    i = i >= getAll.data.length ? 0 : i + 1
+    let random = randomData[Math.floor(Math.random()*randomData.length)]
+    const data = await axios.get(`http://localhost:3000/account/${random}`)
     let payloads = {
       topic: topic,
       messages: [
